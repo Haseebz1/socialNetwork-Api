d790a1612e2d121b80f3ab973db47f376c2d22ba
Haseebz1 <haseebkandore@hotmail.com>
2023-08-07 21:06:44 +0100

    user model created with name, email,  also virtual created for friend count

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const { Schema, Model } = require("mongoose");
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unque: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'must match an email address!'],
+        },
+        thoughts: [
+            {
+                type:Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.lenght;
+});
+const User = Model('User', userSchemaSchema);
+
+model.exports = User;
