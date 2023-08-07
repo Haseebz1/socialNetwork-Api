08ea8044b4213119d87cabe7d69c7884190296b7
Haseebz1 <haseebkandore@hotmail.com>
2023-08-07 13:26:25 +0100

    setting up port and require express frame in server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const mongoose = require ('mongoose');
+
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
