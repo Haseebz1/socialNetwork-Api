4745d59ea97ff74097a3e2969e7a72f6072890a9
Haseebz1 <haseebkandore@hotmail.com>
2023-08-12 11:16:31 +0100

    we added midlleware function and require process.env.port

--- controller/thought-controller.js
+++ controller/thought-controller.js
@@ -10,4 +10,18 @@ const thoughtcontroller = {
       res.status(500).json(err);
     }
   },
+  async getsinglethought(req, res) {
+    try {
+      const dbthoughtData = await Thought.findOne({ _id: req.pram.Thought });
+      if (!dbthoughtData) {
+        return res.status(404).json({ message: "no thought with this id" });
+      }
+      res.json(dbthoughtData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+
 };
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require ('express');
 const mongoose = require ('mongoose');
-
+const routes = require ('./routes');
+const db = require ('./config/connection');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({
+  extended : true
+}));
 app.use(express.json());
+app.use(routes);
 
+db.once('open', () => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+})
+
 });
 
