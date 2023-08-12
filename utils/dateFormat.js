4b214d9f514a3b52ec0dc58eaad6ea3ec8d835bc
Haseebz1 <haseebkandore@hotmail.com>
2023-08-12 11:41:25 +0100

    Part 3 dateformat.js done

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -36,3 +36,31 @@ const addDateSuffix = (date) => {
       10: monthLength === 'short' ? 'Nov' : 'November',
       11: monthLength === 'short' ? 'Dec' : 'December',
     };
+
+    const dateObj = new Date(timestamp);
+  const formattedMonth = months[dateObj.getMonth()];
+
+  const dayOfMonth = dateSuffix
+    ? addDateSuffix(dateObj.getDate())
+    : dateObj.getDate();
+
+  const year = dateObj.getFullYear();
+  let hour =
+    dateObj.getHours() > 12
+      ? Math.floor(dateObj.getHours() - 12)
+      : dateObj.getHours();
+
+  // if hour is 0 (12:00am), change it to 12
+  if (hour === 0) {
+    hour = 12;
+  }
+
+  const minutes = (dateObj.getMinutes() < 10 ? '0' : '') + dateObj.getMinutes();
+
+  // set `am` or `pm`
+  const periodOfDay = dateObj.getHours() >= 12 ? 'pm' : 'am';
+
+  const formattedTimeStamp = `${formattedMonth} ${dayOfMonth}, ${year} at ${hour}:${minutes} ${periodOfDay}`;
+
+  return formattedTimeStamp;
+};
