7b661bbd44990221180ede5ec532814467d5857c
Haseebz1 <haseebkandore@hotmail.com>
2023-08-12 11:27:34 +0100

    made reaction model to reaction schema

--- models/reaction.js
+++ models/reaction.js
@@ -1,24 +1,32 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
 
-const reactionSchema = new Schema({
-  reactionBody: {
-    type: String,
-    required: true,
-    maxlenght: 280,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlenght: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
     createdAt: {
       type: Date,
       default: Date.now,
       get: (timestamp) => dateFormat(timestamp),
     },
   },
-});
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
-
-const Reaction = model("reaction", reactionSchema);
-module.exports = Reaction;
+module.exports = reactionSchema;
